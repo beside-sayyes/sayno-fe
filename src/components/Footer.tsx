3b841f83317c50e105d655786541941ba45b3dfb
gonasooc <doupler@gmail.com>
2024-07-22 12:14:21 +0900

    Feature: 임시 결과 페이지 생성

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div>
+      <img src='images/logo_sayno.png' alt='sayno logo' />
+      <a href='mailto:sayno.cs@gmail.com'>sayno.cs@gmail.com</a>
+      <p>©SAYNO. All rights reserved.</p>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,17 +1,39 @@
-import { useNavigate } from 'react-router-dom';
 import ResultHeader from '../components/ResultHeader.tsx';
+import PrimaryButton from '../components/PrimaryButton.tsx';
+import Footer from '../components/Footer.tsx';
 
 const Result = () => {
-  const navigate = useNavigate();
-
-  const moveBack = () => {
-    navigate(-1);
+  const handleCopyClipBoard = async (text: string) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      alert('클립보드에 링크가 복사되었습니다.');
+    } catch (e) {
+      alert('복사에 실패하였습니다');
+    }
   };
 
   return (
     <div>
       <ResultHeader />
-      Result
+      <div>
+        <h3>먼저, 요청받으신 상황을 해석해드릴게요.</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad architecto commodi dignissimos dolorem ducimus
+          eum eveniet incidunt ipsum maiores maxime nisi non numquam porro quos sed, sit temporibus totam voluptatum?
+        </p>
+      </div>
+      <div>
+        <h3>이렇게 거절하시는거 어떠세요?</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad architecto commodi dignissimos dolorem ducimus
+          eum eveniet incidunt ipsum maiores maxime nisi non numquam porro quos sed, sit temporibus totam voluptatum?
+        </p>
+      </div>
+      <div>
+        <p>거절 멘트 생성 서비스 SAYNO, 친구에게도 추천해주세요</p>
+        <PrimaryButton buttonText={'추천하기'} onClick={() => handleCopyClipBoard('https:/sayno.com')} />
+      </div>
+      <Footer />
     </div>
   );
 };
