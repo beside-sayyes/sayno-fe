c7d116704de28a1d5c0ecfafca1790792d877178
gonasooc <doupler@gmail.com>
2024-07-25 02:22:15 +0900

    Style: 공통 컴포넌트 스타일 누락사항 수정 반영

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,16 +14,18 @@ const Header = ({ onBackClick }: HeaderProps) => {
 
   return (
     <div className={styles.header}>
-      <button className={`default-input ${styles.Button}`} onClick={onBackClick} aria-label={'뒤로 가기'}>
-        <div className={styles.backIconWrapper}>
-          <i className={'icon icon-back'} />
-        </div>
-      </button>
-      <button className={`default-input ${styles.Button}`} onClick={moveHome}>
-        <div className={styles.closeIconWrapper}>
-          <i className={'icon icon-close'} />
-        </div>
-      </button>
+      <div className={styles.inner}>
+        <button className={`default-input ${styles.Button}`} onClick={onBackClick} aria-label={'뒤로 가기'}>
+          <div className={styles.backIconWrapper}>
+            <i className={'icon icon-back'} />
+          </div>
+        </button>
+        <button className={`default-input ${styles.Button}`} onClick={moveHome}>
+          <div className={styles.closeIconWrapper}>
+            <i className={'icon icon-close'} />
+          </div>
+        </button>
+      </div>
     </div>
   );
 };
