811148de02a80ea1ec5746fd0beeb3091dbfd16b
gonasooc <doupler@gmail.com>
2024-09-07 15:05:05 +0900

    Update: cs mail 수정

--- src/components/FooterV2.tsx
+++ src/components/FooterV2.tsx
@@ -6,8 +6,8 @@ const Footer = ({ isV2 = true }) => {
       <div className={styles.imgWrapper}>
         <img className={styles.img} src='/images/logo_sayno_gray.png' alt='sayno logo' />
       </div>
-      <a className={styles.text} href='mailto:cs.just.sayno@gmail.com'>
-        문의: cs.just.sayno@gmail.com
+      <a className={styles.text} href='mailto:help.justsayno@gmail.com'>
+        문의: help.justsayno@gmail.com
       </a>
       <p className={styles.text}>©SAYNO. All rights reserved.</p>
     </footer>
